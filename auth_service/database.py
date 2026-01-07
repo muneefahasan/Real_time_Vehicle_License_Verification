e8a19b5ca4db00fda0a44ad875c72d947fa6f448
muneefahasan <muneefahasan@gmail.com>
2026-01-07 16:44:04 +0530

    remove  Supabase URL in Auth Service

--- auth_service/database.py
+++ auth_service/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # Replace with your actual PostgreSQL connection string
 DATABASE_URL = "postgresql://postgres:20021225@localhost/dmt_users"
 # Supabase URI-ah inge paste pannunga (Password-oda)
-DATABASE_URL = "postgresql://postgres:20021225@db.uxzsljrsotdndfbbseki.supabase.co:5432/postgres"
+#DATABASE_URL = "postgresql://postgres:20021225@db.uxzsljrsotdndfbbseki.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
