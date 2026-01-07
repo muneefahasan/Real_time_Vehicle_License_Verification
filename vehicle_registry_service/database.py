7cea69bf5523512332c1ad74ae65a7834b17b46e
muneefahasan <muneefahasan@gmail.com>
2026-01-07 16:44:41 +0530

    remove supbase url in  Registry Service

--- vehicle_registry_service/database.py
+++ vehicle_registry_service/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual PostgreSQL connection string
 DATABASE_URL = "postgresql://postgres:20021225@localhost/dmt_users"
-DATABASE_URL = "postgresql://postgres:20021225@db.uxzsljrsotdndfbbseki.supabase.co:5432/postgres"
+#DATABASE_URL = "postgresql://postgres:20021225@db.uxzsljrsotdndfbbseki.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
